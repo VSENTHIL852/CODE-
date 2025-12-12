82a96037e4edc79196e16c1b9da0dde7438db488
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2025-12-12 23:32:40 +0530

    Implement string reversal with main method
    Added a main method to test string reversal and modified the reverse logic.

--- ReverseString.java
+++ ReverseString.java
@@ -1,5 +1,13 @@
-class ReverseString {
-    public void reverseString(char[] arr) {
+import java.util.*;
+public class ReverseString{
+    public static void main(String[] args){
+        String str="senthil";
+        String n=rev(str);
+        System.out.print(n);
+
+    }
+    static String rev(String str){
+        char[] arr=str.toCharArray();
         int i=0,j=arr.length-1;
         while(i<j){
             char tmp=arr[i];
@@ -8,7 +16,7 @@ class ReverseString {
             i++;
             j--;
         }
-        System.out.println(arr);
-        
+        return new String(arr);
     }
+    
 }
